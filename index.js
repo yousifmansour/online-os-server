e155d30ced467ebcaa53514332489fa1f6287ebd
Yousif <yousifyacoub1997@gmail.com>
2019-03-28 13:22:54 +0200

    still not working trying something else

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ var app = express();
 var cors = require('cors');
 app.use(cors());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 var privateKey = fs
     .readFileSync('/etc/letsencrypt/live/www.yousifmansour.space/privkey.pem')
