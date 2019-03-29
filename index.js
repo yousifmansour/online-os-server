f56a742c627ea5af4f293a5647973ee3e65b221c
Yousif <yousifyacoub1997@gmail.com>
2019-03-29 12:11:35 +0200

    reenable https

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var routes = require('./routes');
-// var https = require('https');
-var http = require('http');
+var https = require('https');
+// var http = require('http');
 var fs = require('fs');
 
 var seedDB = require('./seedDB');
@@ -17,23 +17,23 @@ app.use(cors());
 // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,
 // Content-Type, Accept");     next(); });
 
-// var privateKey = fs
-//     .readFileSync('/etc/letsencrypt/live/www.yousifmansour.space/privkey.pem')
-//     .toString();
-// var certificate = fs
-//     .readFileSync('/etc/letsencrypt/live/www.yousifmansour.space/fullchain.pem')
-//     .toString();
-// var options = {
-//     key: privateKey,
-//     cert: certificate
-// };
+var privateKey = fs
+    .readFileSync('/etc/letsencrypt/live/www.yousifmansour.space/privkey.pem')
+    .toString();
+var certificate = fs
+    .readFileSync('/etc/letsencrypt/live/www.yousifmansour.space/fullchain.pem')
+    .toString();
+var options = {
+    key: privateKey,
+    cert: certificate
+};
 
 app.use('/', routes);
 
 app.use(express.static('os_apps/Files'));
 
-var server = http.createServer(app);
-// var server = https.createServer(options, app);
+// var server = http.createServer(app);
+var server = https.createServer(options, app);
 
 var setupSocketIo = require('./socketIoConfig');
 setupSocketIo(server);
